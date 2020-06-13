407316ac34c40ac5d5a213ba3fa28726dc8f6b52
Maxwell Ainatchi <maxwell.ainatchi@gmail.com>
2020-06-13 13:54:56 -0400

    fix minor issues with HTML

--- assets/scripts/Main.js
+++ assets/scripts/Main.js
@@ -2,12 +2,7 @@ import MovieDatabase from "./models/MovieDatabase";
 import MovieList from "./views/MovieList";
 import MovieSearcher from "./views/MovieSearcher";
 
-function main() {
-    const movieDatabase = new MovieDatabase();
-    const movieList = new MovieList(movieDatabase);
-    const movieSearcher = new MovieSearcher(movieDatabase);
-    movieList.render();
-}
-
-// Wait for the DOM to load before constructing everything
-window.addEventListener("DOMContentLoaded", main);
+const movieDatabase = new MovieDatabase();
+const movieList = new MovieList(movieDatabase);
+const movieSearcher = new MovieSearcher(movieDatabase);
+movieList.render();
